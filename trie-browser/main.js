d72aad8aa219244304a15b83bc94c0d6078ab412
Vitor Oliveira <vitor.oliveira.24.24@gmail.com>
2021-11-11 01:24:33 -0300

    UI improvements.

--- trie-browser/main.js
+++ trie-browser/main.js
@@ -12,18 +12,32 @@ const addSample = (parent, text) => {
     const cleanText = text.trim()
 
     li.innerText = cleanText
-    li.classList.add('sample')
+    li.classList.add("sample")
     li.id = cleanText
     parent.appendChild(li)
 }
 
 const addSamplesToUI = async (data) => {
     const names = data.map(({ name }) => name)
-    const sampleList = document.getElementById('sample-list')
+    const sampleList = document.getElementById("sample-list")
 
     for (const name of names) {
         addSample(sampleList, name)
         trie.insert(name)
+    }
+}
+
+const toggleSamplesVisibility = (matchText) => {
+    const sampleList = document.getElementById("sample-list")
+
+    for (const sampleElement of sampleList.children) {
+        if (sampleElement.textContent.trim() != matchText) {
+            console.log("Toggle", sampleElement.textContent)
+            sampleElement.classList.toggle("hidden")
+            continue
+        }
+
+        sampleElement.classList.toggle("top-order")
     }
 }
 
@@ -35,31 +49,19 @@ window.onload = async () => {
 // TODO: add interactive trie search method
 (() => {
     const textInput = document.getElementById("text")
+    const searchForm = document.getElementById("search-form")
 
-    textInput.addEventListener('input', (event) => {
-        // console.log(event.data)
-    })
-
-    const searchForm = document.getElementById('search-form')
-
-    searchForm.addEventListener('submit', (event) => {
+    searchForm.addEventListener("submit", (event) => {
         event.preventDefault()
 
         const searchText = textInput.value.trim()
         const found = trie.search(searchText)
 
         if (found) {
-            textInput.setAttribute('disabled', 'disabled')
-
-            const foundElement = document.getElementById(searchText)
-            foundElement.classList.toggle('found')
-
-            setTimeout(() => {
-                foundElement.classList.toggle('found')
-                textInput.removeAttribute('disabled')
-            }, 1000)
+            toggleSamplesVisibility(searchText)
+            setTimeout(() => toggleSamplesVisibility(searchText), 1500)
         }
 
-        textInput.value = ''
+        textInput.value = ""
     })
 })()
